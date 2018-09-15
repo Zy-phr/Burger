4d5f3bf995ac6a6881818ad7bb7983b809d1df0c
Zy-phr <richard.sanchez79@yahoo.com>
2018-09-14 20:44:56 -0500

    Heroku

---
+++ server.js
@@ -0,0 +1,30 @@
+// =====================================================
+// connection.js
+// =====================================================
+
+const Mysql = require("mysql");
+
+// connection configuration
+
+let config;
+
+// db connection config using JawsDB
+
+if (process.env.JAWSDB_URL) {
+	config = process.env.JAWSDB_URL;
+}
+else {
+
+    // default db config for local db
+    
+	config = {
+		host: "localhost",
+		user: "",
+		password: "",
+		database: "burgers_db"
+	};
+}
+
+// export connection to burgers_db
+
+module.exports = Mysql.createConnection(config);
